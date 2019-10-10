e9b03eae1a9506622cbeb72dd9a4cbe843ec3e25
Sheikh Sharfuddin Mim <sharfuddin730@gmail.com>
2019-10-10 12:30:25 +0200

    commit

---
+++ AppM4-ngFor/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  courses= [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'}
+  ]
+}
