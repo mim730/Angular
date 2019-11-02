c14842490753371f56b7722fcd3e8e149303681d
Sheikh Sharfuddin Mim <sharfuddin730@gmail.com>
2019-11-02 11:33:22 +0100

    commit

--- Projects/Vesco/js/custom.js
+++ Projects/Vesco/js/custom.js
@@ -7,3 +7,36 @@ $(document).ready(function () {
     // animate on scroll
     new WOW().init();
 });
+
+
+
+
+/*=================================
+            WORK
+==================================*/
+
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery: {
+            enabled: true
+        }
+    });
+});
+
+
+/*=================================
+            TEAM
+==================================*/
+
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
